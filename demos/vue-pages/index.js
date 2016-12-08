a823146a2090bbfb656f46e4c89d3a34dd8889cc
cc <ccking@foxmail.com>
2016-12-08 14:46:43 +0800

    update the new vue-pages demo 😈 🎻

--- demos/vue-pages/index.js
+++ demos/vue-pages/index.js
@@ -7,7 +7,7 @@ new Vue({
     return {
       url1: '#',
       url2: '?param=pages',
-      pageName:'p',
+      pageParam:'page',
       total: 27,
       counts: 10,
       current1: 11,
@@ -16,12 +16,10 @@ new Vue({
   },
   methods: {
     fn1(d, e){
-      console.log('demo1: ' + d)
       this.current1 = d
     },
     fn2(d, e){
       e.preventDefault()
-      console.log('demo2: ' + d)
       this.current2 = d
     }
   },
